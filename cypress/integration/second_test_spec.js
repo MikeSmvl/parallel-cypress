b8f95a466d7845effac4d99ef0d86295977ec7af
Mikael Samvelian <mikael.samvelian@gmail.com>
2022-04-12 16:53:05 -0700

    Update workflow with matrix

---
+++ cypress/integration/second_test_spec.js
@@ -0,0 +1,14 @@
+describe('My Second Test', () => {
+  it('clicking "type" shows the right headings', () => {
+    cy.wait(10000)
+    cy.visit('https://example.cypress.io')
+    cy.pause()
+    cy.contains('type').click()
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
